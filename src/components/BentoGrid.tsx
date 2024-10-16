03ad545095fa792676a23aa2265922d06429f29a
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-16 11:55:47 +0530

    Third Page also completed ✅.

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function BentoGrid() {
   return (
-    <div className='flex justify-between items-center gap-10'>
+    <div className='flex justify-between items-center gap-5'>
         <div className='w-[500px] h-[550px] bg-bentogrid-bg rounded-xl flex flex-col justify-between'>
             <div className='pt-[40px] px-[40px]'>
                 <p className='text-white text-desktop/h4 font-[MARK_OT]'>Amp Your Style with itel Latest</p>
--- src/components/Third.tsx
+++ src/components/Third.tsx
@@ -4,13 +4,13 @@ import BentoGrid from './BentoGrid'
 function Third() {
   return (
     <>
-        <div className='w-full h-screen bg-third-bg flex flex-row justify-center'>
+        <div className='w-full h-screen bg-third-bg flex flex-row justify-center py-[72px]'>
             <div className='relative'>
-                <img src="/EnjoyBetterLife.png" alt="" className='w-full h-44 '/>
-                <div className='px-20'>
+                <img src="/EnjoyBetterLife.png" alt="" className='w-full h-48 relative z-0'/>
+                <div className='px-20 w-full absolute z-10 top-24 flex justify-center'>
                     <BentoGrid/>
                 </div>
-                <img src="/EnjoyBetterLife2.png" alt="" className='w-full h-44'/>
+                <img src="/EnjoyBetterLife2.png" alt="" className='w-full h-48 absolute z-0 bottom-0'/>
             </div>
         </div>
     </>
