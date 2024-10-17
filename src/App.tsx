00cb42a1540ca12dc308e4e485cefaa31d995b81
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-17 22:31:16 +0530

    vercel error solved .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Enjoy from "./components/Enjoy"
 import HotPicks from "./components/HotPicks"
 import First from "./components/pages/First"
-import Third from "./components/pages/Third"
 
 function App() {
 
