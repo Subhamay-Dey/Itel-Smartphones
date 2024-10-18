9c9b8950dd8ed496f1b3f239004ca0dced5f8084
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-18 22:05:25 +0530

    updated.

--- src/components/it2175p/Thirdpage.tsx
+++ src/components/it2175p/Thirdpage.tsx
@@ -1,22 +1,30 @@
 
 function Thirdpage() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center bg-SnapandSaveBG bg-center bg-cover min-h-screen px-4 md:px-10">
+    <div className="md:flex flex flex-col-reverse md:flex-row items-center justify-center lg:bg-SnapandSaveBG bg-SnapandSaveBGmobile bg-center bg-cover min-h-screen px-4 md:px-10">
         <section className="flex-1 flex items-end justify-center">
             <div className="">
-                <img src="/it2175/backgrounds/SnapandSaveGraphic.png" alt="" className="h-screen pl-16 pt-16"/>
+                <img src="/it2175/backgrounds/SnapandSaveGraphic.png" alt="" className="md:h-[500px] md:w-[600px] lg:h-screen h-[500px] lg:pl-16 lg:pt-16"/>
             </div>
         </section>
-        <section className="flex-1 flex flex-col w-full h-screen items-start justify-center p-4 md:pt-0 pt-10">
+        <section className="flex-1 flex flex-col w-full h-screen items-start justify-center md:p-4 md:pt-0 pt-10">
             <div className="w-full h-[60%] flex justify-start items-start">
-                <div className="text-white pl-32">
+                <div className="text-white pl-32 hidden lg:block">
                     <h1 className="text-3xl md:text-4xl lg:text-[60px] font-bold leading-[72px] tracking-[0.03em] text-left py-4">
                         Snap & Save: Rear
                     </h1>
                     <h1 className="text-3xl md:text-4xl lg:text-[60px] font-bold leading-[72px] tracking-[0.03em] text-left py-4">
                         Camera Magic
                     </h1>
-                    <p className="mt-4 text-base md:text-lg text-left w-[60%] flex-wrap pt-4">
+                    <p className="mt-4 text-base md:text-lg text-left w-[65%] flex-wrap pt-4">
+                        Snap precious moments with itel it2175 Pro's rear camera - Memories captured, connections preserved.
+                    </p>
+                </div>
+                <div className="text-white px-4 md:hidden lg:hidden block">
+                    <h1 className="text-mobile/h3 font-[MARK_OT] pb-4">
+                        Snap & Save: Rear Camera Magic
+                    </h1>
+                    <p className="font-[Mark_OT] text-[16px] leading-[19.6px] tracking-[0.01em] text-left">
                         Snap precious moments with itel it2175 Pro's rear camera - Memories captured, connections preserved.
                     </p>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -242,7 +242,8 @@ export default {
 				"whiteButton" : "url('/whitebutton.png')",
 				"the20displayBG" : "url('/it2175/backgrounds/the20displayBG.webp')",
 				"powerthatgoeasBG" : "url('/it2175/backgrounds/powerthatgoeasBG.webp')",
-				"SnapandSaveBG" : "url('/it2175/backgrounds/SnapandSaveBG.webp')"
+				"SnapandSaveBG" : "url('/it2175/backgrounds/SnapandSaveBG.webp')",
+				"SnapandSaveBGmobile" : "url('/it2175/backgrounds/SnapandSaveBGmobile.webp')",
 			},
 			borderImage: {
 				's24-gradient-border': 'linear-gradient(107.83deg, #FFCF92 23.83%, #FFE8CB 75.66%) 1',
