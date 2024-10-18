97b12ade6389313b18def726b1c5f581150a5cb5
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-18 16:19:50 +0530

    first page done ✅.

--- src/components/it2175p/Firstpage.tsx
+++ src/components/it2175p/Firstpage.tsx
@@ -12,11 +12,11 @@ const Firstpage = () => {
     //   </div>
 
     // </div>
-<div className="flex justify-center items-center min-h-screen bg-the20displayBG bg-cover bg-center">
-    <div className="flex flex-col-reverse lg:flex-row w-full max-w-7xl h-full lg:h-[80vh] mx-auto md:p-4 px-4 pt-4">
+<div className="flex justify-evenly items-center min-h-screen bg-the20displayBG bg-cover bg-center">
+    <div className="flex flex-col-reverse lg:flex-row w-full max-w-8xl h-full mx-auto lg:mx-2 md:p-4 px-4 pt-4 lg:gap-4">
         {/* Phone Images Section */}
         <section className="flex justify-center items-end w-full lg:w-3/5 h-full">
-            <div className="w-full h-full max-w-[950px] max-h-[600px] lg:max-h-[80vh] flex items-center justify-center">
+            <div className="w-full h-full max-w-[950px] max-h-[600px] lg:max-h-[80vh] lg:max-w-[100vh] flex items-center justify-center">
                 <img 
                     src="/it2175/backgrounds/the2.0displayGraphic.webp" 
                     alt="displayGraphic" 
@@ -26,31 +26,40 @@ const Firstpage = () => {
         </section>
 
         {/* Writing Section */}
-        <section className="flex flex-col justify-center w-full lg:w-2/5 p-4 h-full lg:h-auto">
-            <h1 className="text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-4 text-center lg:text-left">
+        <section className="flex flex-col justify-center w-full lg:w-1/3 p-4 h-full lg:h-auto ">
+            <h1 className="lg:hidden text-4xl xl:text-5xl 2xl:text-6xl mb-4 text-center lg:text-left text-white text-mobile/h4 font-[MARK_OT]">
                 The 2.0” Display Advantage
             </h1>
-            <p className="text-lg xl:text-xl 2xl:text-2xl mb-4 text-center lg:text-left">
+        <div className="lg:flex lg:flex-col hidden lg:pb-6">
+            <h1 className="text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-4 text-center lg:text-left text-white">
+                The 2.0” Display
+            </h1>
+            <h1 className="text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-4 text-center lg:text-left text-white">
+                Advantage
+            </h1>
+        </div>
+            {/* <p className="text-lg xl:text-xl 2xl:text-2xl mb-4 text-center lg:text-left"> */}
+            <p className="font-[MARK_OT] text-[20px] leading-[22.72px] text-left lg:py-2 text-white text-mobile/body/2">
                 Experience a bigger view and elevated style with itel2175 Pro's big and bright 5.1cm (2”) Display. Ergonomically designed for your comfort.
             </p>
 
             {/* Centering the option group */}
-            <div className="grid grid-cols-2 gap-4 justify-center">
-                <div className="flex items-center justify-center">
-                    <i className="fas fa-thumbs-up text-2xl xl:text-3xl 2xl:text-4xl mr-2"></i>
-                    <p className="text-lg xl:text-xl 2xl:text-2xl">Bigger View</p>
+            <div className="grid grid-cols-2 gap-4 gap-y-8 lg:py-6 justify-center">
+                <div className="flex items-center justify-start">
+                    <img src="/it2175/backgrounds/hand-point.png" alt="hand" className="text-lg xl:text-3xl 2xl:text-4xl mr-2"/>
+                    <p className="text-lg xl:text-xl 2xl:text-2xl text-white lg:text-desktop/h5/medium text-mobile/h7 font-[MARK_OT]">Bigger View</p>
                 </div>
-                <div className="flex items-center justify-center">
-                    <i className="fas fa-thumbs-up text-2xl xl:text-3xl 2xl:text-4xl mr-2"></i>
-                    <p className="text-lg xl:text-xl 2xl:text-2xl">Elevated Style</p>
+                <div className="flex items-center justify-start">
+                    <img src="/it2175/backgrounds/hand-point.png" alt="hand" className="fas fa-thumbs-up text-2xl xl:text-3xl 2xl:text-4xl mr-2"/>
+                    <p className="text-lg xl:text-xl 2xl:text-2xl text-white lg:text-desktop/h5/medium text-mobile/h7 font-[MARK_OT]">Elevated Style</p>
                 </div>
-                <div className="flex items-center justify-center">
-                    <i className="fas fa-thumbs-up text-2xl xl:text-3xl 2xl:text-4xl mr-2"></i>
-                    <p className="text-lg xl:text-xl 2xl:text-2xl">Highly Ergonomic</p>
+                <div className="flex items-center justify-start">
+                    <img src="/it2175/backgrounds/hand-point.png" alt="hand" className="fas fa-thumbs-up text-2xl xl:text-3xl 2xl:text-4xl mr-2"/>
+                    <p className="text-lg xl:text-xl 2xl:text-2xl text-white lg:text-desktop/h5/medium text-mobile/h7 font-[MARK_OT]">Highly Ergonomic</p>
                 </div>
-                <div className="flex items-center justify-center">
-                    <i className="fas fa-thumbs-up text-2xl xl:text-3xl 2xl:text-4xl mr-2"></i>
-                    <p className="text-lg xl:text-xl 2xl:text-2xl">Elevated Experiences</p>
+                <div className="flex items-center justify-start">
+                    <img src="/it2175/backgrounds/hand-point.png" alt="hand" className="fas fa-thumbs-up text-2xl xl:text-3xl 2xl:text-4xl mr-2"/>
+                    <p className="text-lg xl:text-xl 2xl:text-2xl text-white lg:text-desktop/h5/medium text-mobile/h7 font-[MARK_OT]">Elevated Experiences</p>
                 </div>
             </div>
         </section>
