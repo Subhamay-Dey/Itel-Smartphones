63b18a5e543b35091c2c2c0bec7c50c84461d924
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-21 23:47:23 +0530

    Home page done ✅.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/it2175p/Home'
+
+const It5262 = () => {
+    return (
+        <div>
+            <Hero {...HeroDatait5262} />
+        </div>
+    )
+}
+
+export default It5262
