1413808141d654d3c131b07cfc7e557d90768102
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-22 19:08:19 +0530

    Futurerich section completed ✅.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 import Hero from './components/it2175p/Home'
 import StickyHeader from './components/it2175p/StickyHeader'
 import { FeatureRichDatait5262, FullSpecificationsDatait5262, HeroDatait5262, KingVoiceDatait5262, ProductGalleryDataIt5262, TechSpecsDatait5262 } from '../../data/featurephonedata/it2175Data'
+import FeatureRich from './components/it2175p/FeatureRich'
 
-const It5262 = () => {
+const It2175 = () => {
     return (
         <div>
             <Hero {...HeroDatait5262} />
             <StickyHeader {...HeroDatait5262} />
+            <FeatureRich {...FullSpecificationsDatait5262}/>
         </div>
     )
 }
 
-export default It5262
+export default It2175
