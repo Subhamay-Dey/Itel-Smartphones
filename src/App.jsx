f9f39bd8156336fdf9a39d15946da3e7d370e290
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-22 19:27:11 +0530

    PhoneSliderAnimation done✅

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/it2175p/Home'
 import StickyHeader from './components/it2175p/StickyHeader'
 import { FeatureRichDatait5262, FullSpecificationsDatait5262, HeroDatait5262, KingVoiceDatait5262, ProductGalleryDataIt5262, TechSpecsDatait5262 } from '../../data/featurephonedata/it2175Data'
 import FeatureRich from './components/it2175p/FeatureRich'
+import PhoneSliderAnimation from './components/it2175p/PhoneSliderAnimation '
 
 const It2175 = () => {
     return (
@@ -11,6 +12,7 @@ const It2175 = () => {
             <Hero {...HeroDatait5262} />
             <StickyHeader {...HeroDatait5262} />
             <FeatureRich {...FullSpecificationsDatait5262}/>
+            <PhoneSliderAnimation/>
         </div>
     )
 }
