82468d2c5d61dc50ef9ca64bcf619aeb1ba0f2b8
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-25 01:19:26 +0530

    SmartApp section finally completed ✅.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HeroSection from './components/Homepage/pages/Hero'
 import Homepage from './components/Homepage/Homepage'
 import HotPicks from './components/Homepage/HotPicks'
 import CoolGadgets from './components/Homepage/CoolGadgets'
+import SmartApp from './components/Homepage/SmartApp'
 
 const It2175 = () => {
     return (
@@ -11,6 +12,7 @@ const It2175 = () => {
             <Homepage/>
             <HotPicks/>
             <CoolGadgets/>
+            <SmartApp/>
         </div>
     )
 }
