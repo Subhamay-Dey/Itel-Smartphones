20afb967b316cea31b270fcc744e612073c451c3
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-25 04:04:52 +0530

    Entertainment section completed ✅.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 
-import HeroSection from './components/Homepage/pages/Hero'
 import Homepage from './components/Homepage/Homepage'
 import HotPicks from './components/Homepage/HotPicks'
 import CoolGadgets from './components/Homepage/CoolGadgets'
 import SmartApp from './components/Homepage/SmartApp'
 import Enjoy from './components/Homepage/Enjoy'
+import Entertainment from './components/Homepage/Entertainment'
+
 
 const It2175 = () => {
     return (
         <div>
-            <HeroSection/>
             <Homepage/>
             <HotPicks/>
+            <Entertainment/>
             <CoolGadgets/>
             <SmartApp/>
             <Enjoy/>
