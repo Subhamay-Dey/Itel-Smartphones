c667683109e0ffca84139e56b4c190a53de5bb4a
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-25 08:36:28 +0530

    minor responsive changes ✅.

--- src/components/Homepage/SmartApp.tsx
+++ src/components/Homepage/SmartApp.tsx
@@ -6,8 +6,8 @@ function SmartApp() {
         <div className="content flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-row sm:flex-col flex-col lg:py-10 md:py-16 md:px-10 py-6 px-0 lg:px-12 xl:px-0 2xl:px-0 w-full justify-between items-center gap-4 lg:gap-6 md:gap-4">
             <div className="">
                 <div className="relative text-white lg:px-10 md:px-4 px-4 w-full">
-                    <p className="absolute lg:top-6 md:top-3 top-4 md:text-desktop/h3 text-desktop/h4">Home Care</p>
-                    <p className="absolute lg:top-20 md:top-16 top-12 md:text-desktop/body/1 text-desktop/body/2/regular flex flex-wrap md:w-9/10 lg:w-3/4 w-8/10">Enhance Home Care by automating tasks, improving efficiency and ensuring convinience</p>
+                    <p className="absolute lg:top-6 md:top-3 top-6 md:text-desktop/h3 text-desktop/h4">Home Care</p>
+                    <p className="absolute lg:top-20 md:top-16 top-20 md:text-desktop/body/1 text-desktop/body/2 flex flex-wrap md:w-9/10 lg:w-3/4 w-8/10">Enhance Home Care by automating tasks, improving efficiency and ensuring convinience</p>
                 </div>
                 
                 <img src="/homepage/Dryer.gif" alt="Dryer" className="md:max-w-full md:max-h-[620px] hidden md:flex"/>
@@ -16,8 +16,8 @@ function SmartApp() {
             </div>
             <div className="">
                 <div className="relative text-white lg:px-10 md:px-4 px-4 w-full">
-                    <p className="absolute lg:top-6 md:top-3 top-4 md:text-desktop/h3 text-desktop/h4">Personal Care</p>
-                    <p className="absolute lg:top-20 md:top-16 top-12 md:text-desktop/body/1 text-desktop/body/2/regular flex flex-wrap md:w-9/10 lg:w-3/4 w-8/10">Innovative smart hair dryers feature AI temperature control and customizable settings</p>
+                    <p className="absolute lg:top-6 md:top-3 top-6 md:text-desktop/h3 text-desktop/h4">Personal Care</p>
+                    <p className="absolute lg:top-20 md:top-16 top-20 md:text-desktop/body/1 text-desktop/body/2 flex flex-wrap md:w-9/10 lg:w-3/4 w-8/10">Innovative smart hair dryers feature AI temperature control and customizable settings</p>
                 </div>
                 
                 <img src="/homepage/VCleanr.gif" alt="VCleanr" className="md:max-w-full md:max-h-[620px] hidden md:flex"/>
